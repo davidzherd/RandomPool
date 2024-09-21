44d92bd396e688ec48e2d98d28e1cd4cbd913f54
davidzherd <dzherdenovsky+github@gmail.com>
2024-09-21 16:24:38 +0300

    clear code

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
-import { useState } from 'react';
 import './global.css';
 import GameSettings from './pages/GameSettings';
-interface Player{
-  playerName: string;
-  playerBalls: number[];
-}
 import Home from './pages/Home';
+
 function App() {
-  const [gameSettings, setGameSettings] = useState({numOfPlayers:2, players:[]});
   return (
     // <Home/>
     <GameSettings/>
