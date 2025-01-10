721cd6b1198ae9a964cfd511962ed0509b554440
davidzherd <dzherdenovsky+github@gmail.com>
2025-01-10 12:06:46 +0200

    fixed overflow issues

--- src/pages/GameSettings.tsx
+++ src/pages/GameSettings.tsx
@@ -1,14 +1,14 @@
-import { FormEvent, useContext, useState } from 'react';
-import { Card } from '../components/Card';
-import { Center } from '../components/Center';
-import { Dropdown } from '../components/Dropdown';
-import { Input } from '../components/Input';
-import { Logo } from '../components/Logo';
-import { Text } from '../components/Text';
-import { StartButton } from '../components/Button';
-import { createPlayers, formValidation } from '../logic';
-import { Navigate } from 'react-router-dom';
-import { PlayersContext } from '../App';
+import { FormEvent, useContext, useState } from "react";
+import { Card } from "../components/Card";
+import { Center } from "../components/Center";
+import { Dropdown } from "../components/Dropdown";
+import { Input } from "../components/Input";
+import { Logo } from "../components/Logo";
+import { Text } from "../components/Text";
+import { StartButton } from "../components/Button";
+import { createPlayers, formValidation } from "../logic";
+import { Navigate } from "react-router-dom";
+import { PlayersContext } from "../App";
 
 function GameSettings() {
   const playerCtx = useContext(PlayersContext);
@@ -20,53 +20,80 @@ function GameSettings() {
   const submitDetails = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const valid = formValidation(e.currentTarget.elements);
-    if (valid){
+    if (valid) {
       const players = createPlayers(e.currentTarget.elements, numOfBalls);
-      playerCtx.updatePlayersData(players)
-      playerCtx.updateInitialPlayersData({players: [...players], balls: numOfBalls})
+      playerCtx.updatePlayersData(players);
+      playerCtx.updateInitialPlayersData({
+        players: [...players],
+        balls: numOfBalls,
+      });
       setSettingSaved(true);
-    }else{
+    } else {
       setValidForm(false);
     }
   };
 
   return (
     <>
-    {settingSaved && <Navigate to={'/RandomPool/game'}/>}
+      {settingSaved && <Navigate to={"/RandomPool/game"} />}
       <form onSubmit={submitDetails}>
-        <Center direction='column'>
+        <Center direction="column" style={{ overflow: "auto" }}>
           <Logo />
           <Text shadowcolor="#ed428e" size={2} weight="400">
             Let's start a new game!
           </Text>
-          <Card width='90%' margin="1rem" padding='0.5rem' direction='column'>
-            <div style={{display: "flex", gap: "0.5rem"}}>
-            <Text color='#919191' weight="500">
-              Number of players:
-            </Text>
-            <Dropdown selected={numOfPlayers} menuItems={[2, 3, 4, 5]} action={(number: number) => setNumOfPlayers(number)} />
+          <Card width="90%" margin="1rem" padding="0.5rem" direction="column">
+            <div style={{ display: "flex", gap: "0.5rem" }}>
+              <Text color="#919191" weight="500">
+                Number of players:
+              </Text>
+              <Dropdown
+                selected={numOfPlayers}
+                menuItems={[2, 3, 4, 5]}
+                action={(number: number) => setNumOfPlayers(number)}
+              />
             </div>
-            <div style={{display: "flex", gap: "0.5rem"}}>
-            <Text color='#919191' weight="500">
-              Number of balls per player:
-            </Text>
-            <Dropdown selected={numOfBalls} menuItems={[1,2,3]} action={(number: number) => setNumOfBalls(number)} />
+            <div style={{ display: "flex", gap: "0.5rem" }}>
+              <Text color="#919191" weight="500">
+                Number of balls per player:
+              </Text>
+              <Dropdown
+                selected={numOfBalls}
+                menuItems={[1, 2, 3]}
+                action={(number: number) => setNumOfBalls(number)}
+              />
             </div>
             {Array.from({ length: numOfPlayers }, (_, index) => (
-              <div style={{ display: "flex" }} key={index.toString() + "container"}>
+              <div
+                style={{ display: "flex" }}
+                key={index.toString() + "container"}
+              >
                 <div key={index.toString() + "name"}>
-                  <Text color='#919191' weight="500">Player name:</Text>
-                  <Input color='whitesmoke' width='70%' />
+                  <Text color="#919191" weight="500">
+                    Player name:
+                  </Text>
+                  <Input color="whitesmoke" width="70%" />
                 </div>
                 <div key={index.toString() + "pass"}>
-                  <Text color='#919191' weight="500">Player password:</Text>
-                  <Input type='password' color='whitesmoke' width='70%' />
+                  <Text color="#919191" weight="500">
+                    Player password:
+                  </Text>
+                  <Input type="password" color="whitesmoke" width="70%" />
                 </div>
               </div>
             ))}
           </Card>
-          {!validForm && <Text style={{color: "red"}}>All fields must be filled.</Text>}
-          <StartButton type='submit' btncolor='#ed428e' fill={false} txtcolor='#ed428e' size={2}>
+          {!validForm && (
+            <Text style={{ color: "red" }}>All fields must be filled.</Text>
+          )}
+          <StartButton
+            type="submit"
+            btncolor="#ed428e"
+            fill={false}
+            txtcolor="#ed428e"
+            size={2}
+            style={{ position: "static" }}
+          >
             START GAME
           </StartButton>
         </Center>
